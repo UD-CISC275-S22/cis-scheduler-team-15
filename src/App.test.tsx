d8f9cbafe736fc855ca20332a84832da01fcd645
Henry Fidlow <henryrockford36@yahoo.com>
2022-04-17 18:55:50 -0400

    Added tests for inserting and viewing semesters

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("Final Project Tests", () => {
     beforeEach(() => {
@@ -158,13 +159,56 @@ describe("Final Project Tests", () => {
             name: /1: Default/i
         });
         firstDefault?.click();
+        const text1 = screen.queryByText("Fall 2020");
+        const text2 = screen.queryByText("Spring 2022");
+        const text3 = screen.queryByText("CISC108");
+        const text4 = screen.queryByText("Introduction to Computer Science I");
+        const text5 = screen.queryAllByText("Total Credits");
+        const text6 = screen.queryByText("Default Total Credits: 121");
+        expect(text1).toBeVisible();
+        expect(text2).toBeVisible();
+        expect(text3).toBeVisible();
+        expect(text4).toBeVisible();
+        expect(text5[0]).toBeVisible();
+        expect(text6).toBeVisible();
     });
 
-    test("Students can insert or remove a semester in a plan", () => {});
-
+    test("Students can insert or remove a semester in a plan", () => {
+        const firstDefault = screen.queryByRole("button", {
+            name: /1: Default/i
+        });
+        firstDefault?.click();
+        let text1 = screen.queryByText("Winter 2022");
+        let text2 = screen.queryAllByText("Fall 2022");
+        expect(text1).toBeNull();
+        expect(text2[1]).toBeUndefined();
+        expect(text2[0]).not.toBeUndefined();
+        const editPlan = screen.queryByRole("button", {
+            name: /Edit Plan/i
+        });
+        editPlan?.click();
+        const addSemester = screen.queryByRole("button", {
+            name: /Add Semester/i
+        });
+        addSemester?.click();
+        const createSemester = screen.queryByRole("button", {
+            name: /Create Semester/i
+        });
+        createSemester?.click();
+        text2 = screen.queryAllByText("Fall 2022");
+        expect(text2[1]).toBeUndefined();
+        expect(text2[0]).not.toBeUndefined();
+        const seasonButton = screen.getAllByRole("combobox");
+        userEvent.selectOptions(seasonButton[0], "Winter");
+        createSemester?.click();
+        text1 = screen.queryByText("Winter 2022");
+        expect(text1).not.toBeNull();
+    });
+    /*
     test("Students can insert a course in a semester", () => {});
 
     test("Students can clear out semesters in a plan", () => {});
 
     test("Students can clear out all the existing courses in a semester or clear out individual courses", () => {});
+    */
 });
