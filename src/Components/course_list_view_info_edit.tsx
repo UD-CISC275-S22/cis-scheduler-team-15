635a48c03ba12241e084a270e312afe58ae1cde4
Michael Savas <mjsavas9@gmail.com>
2022-04-18 23:32:28 -0400

    Able to add and remove prereqs

--- src/Components/course_list_view_info_edit.tsx
+++ src/Components/course_list_view_info_edit.tsx
@@ -20,20 +20,31 @@ export function CourseListViewInfoEdit({
 }): JSX.Element {
     const [edit, setEdit] = useState<boolean>(false);
     const [creditse, setCredits] = useState<number>(course.credits);
-    const [listing, setListing] = useState<string>(course.listing);
-    const [title, setTitle] = useState<string>(course.title);
+    const [listings, setListing] = useState<string>(course.listing);
+    const [titles, setTitle] = useState<string>(course.title);
+    const [preReqss, setPreReqs] = useState<number[]>(course.preReqs);
+    const [newID, setNewID] = useState<number>(idInitialSetter);
+    const [removeID, setRemoveID] = useState<number>(idInitialSetter);
+    const [double, setDouble] = useState<boolean>(false);
+    const [doubler, setDoubler] = useState<boolean>(false);
 
-    function createNewCourse(course: Course): Course {
-        courses;
+    function idInitialSetter(): number {
+        if (course.courseID === 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+    function createNewCourse(coursen: Course): Course {
         const newCourse: Course = {
-            courseID: course.courseID,
-            listing: listing,
-            title: title,
-            preReqs: course.preReqs,
-            coReqs: course.coReqs,
-            offered: course.offered,
+            courseID: coursen.courseID,
+            listing: listings,
+            title: titles,
+            preReqs: preReqss,
+            coReqs: coursen.coReqs,
+            offered: coursen.offered,
             credits: creditse,
-            reqsSatisfied: course.reqsSatisfied
+            reqsSatisfied: coursen.reqsSatisfied
         };
         return newCourse;
     }
@@ -49,12 +60,69 @@ export function CourseListViewInfoEdit({
     }
     function resetDefault(courseID: number) {
         const defaultCourse: Course[] = defaultCourseData.filter(
-            (course: Course) => courseID === course.courseID
+            (coursed: Course) => courseID === coursed.courseID
         );
-        editCourses(courseID, defaultCourse[0]);
         setTitle(defaultCourse[0].title);
         setListing(defaultCourse[0].listing);
         setCredits(defaultCourse[0].credits);
+        setPreReqs(defaultCourse[0].preReqs);
+        editCourses(courseID, defaultCourse[0]);
+        setDouble(false);
+        setDoubler(false);
+    }
+    function updateNewID(event: ChangeEvent) {
+        setNewID(parseInt(event.target.value));
+    }
+    function updateRemoveID(event: ChangeEvent) {
+        setRemoveID(parseInt(event.target.value));
+    }
+
+    function addNewPreReq(courseID: number) {
+        if (preReqss.includes(courseID)) {
+            setDouble(true);
+            setDoubler(false);
+        } else {
+            setPreReqs([...preReqss, courseID]);
+            setDouble(false);
+            setDoubler(false);
+            const newCourse: Course = {
+                courseID: course.courseID,
+                listing: listings,
+                title: titles,
+                preReqs: [...preReqss, courseID],
+                coReqs: course.coReqs,
+                offered: course.offered,
+                credits: creditse,
+                reqsSatisfied: course.reqsSatisfied
+            };
+            editCourses(course.courseID, newCourse);
+        }
+    }
+
+    function removePreReq(courseID: number) {
+        if (preReqss.includes(courseID)) {
+            const removePreReq = preReqss.filter(
+                (courseIDf: number) => courseIDf !== courseID
+            );
+            setDouble(false);
+            const newCourse: Course = {
+                courseID: course.courseID,
+                listing: listings,
+                title: titles,
+                preReqs: removePreReq,
+                coReqs: course.coReqs,
+                offered: course.offered,
+                credits: creditse,
+                reqsSatisfied: course.reqsSatisfied
+            };
+            editCourses(course.courseID, newCourse);
+            setPreReqs(removePreReq);
+            setDoubler(false);
+            setDouble(false);
+        } else {
+            setDoubler(true);
+            setDouble(false);
+        }
     }
 
     return (
@@ -89,7 +157,7 @@ export function CourseListViewInfoEdit({
                         <InputGroup>
                             <Form.Control
                                 placeholder="Enter new course title"
-                                value={title}
+                                value={titles}
                                 onChange={updateTitle}
                             />
                             <Button
@@ -107,7 +175,7 @@ export function CourseListViewInfoEdit({
                         <InputGroup>
                             <Form.Control
                                 placeholder="Enter new listing"
-                                value={listing}
+                                value={listings}
                                 onChange={updateListing}
                             />
                             <Button
@@ -121,6 +189,50 @@ export function CourseListViewInfoEdit({
                                 Update Listing
                             </Button>
                         </InputGroup>
+                        <Form.Label>Edit Pre-Requisites:</Form.Label>
+                        <InputGroup>
+                            <Form.Select value={newID} onChange={updateNewID}>
+                                {courses
+                                    .filter(
+                                        (coursef: Course) =>
+                                            coursef.courseID !== course.courseID
+                                    )
+                                    .map((coursem: Course) => (
+                                        <option
+                                            key={coursem.courseID}
+                                            value={coursem.courseID}
+                                        >
+                                            {coursem.listing}
+                                        </option>
+                                    ))}
+                            </Form.Select>
+                            <Button onClick={() => addNewPreReq(newID)}>
+                                Add PreReq
+                            </Button>
+                            <Form.Select
+                                value={removeID}
+                                onChange={updateRemoveID}
+                            >
+                                {courses
+                                    .filter(
+                                        (coursef: Course) =>
+                                            coursef.courseID !== course.courseID
+                                    )
+                                    .map((coursem: Course) => (
+                                        <option
+                                            key={coursem.courseID}
+                                            value={coursem.courseID}
+                                        >
+                                            {coursem.listing}
+                                        </option>
+                                    ))}
+                            </Form.Select>
+                            <Button onClick={() => removePreReq(removeID)}>
+                                Remove PreReq
+                            </Button>
+                        </InputGroup>
+                        <div>{double && "Course already a PreReq"}</div>
+                        <div>{doubler && "Course not one of the PreReqs"}</div>
                         <hr />
                         <Button onClick={() => resetDefault(course.courseID)}>
                             Reset Course to Default
