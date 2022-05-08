d07a7d89f9ee40444a50cebf1999bcc1dfbc9c2d
Henry Fidlow <henryrockford36@yahoo.com>
2022-05-08 15:26:10 -0400

    Fixed Export to CSV functionality

--- src/Components/degree_plan_view.tsx
+++ src/Components/degree_plan_view.tsx
@@ -49,8 +49,95 @@ export function DegreePlanView({
         });
     }
 
+    function arrayToCSV(): string {
+        /*
+        const headers = [
+            { label: "DegreeID", key: "degreeID" },
+            { label: "Name", key: "name" },
+            { label: "SemesterID", key: "semesterID" },
+            { label: "Season", key: "season" },
+            { label: "Year", key: "year" },
+            { label: "Semester", key: "semesterID" },
+            { label: "CourseID", key: "courseID" },
+            { label: "Course Listing", key: "listing" },
+            { label: "Course Title", key: "title" },
+            { label: "PreReqs", key: "preReqs" },
+            { label: "CoReqs", key: "coReqs" },
+            { label: "Seasons Offered", key: "offered" },
+            { label: "Credits", key: "credits" },
+            { label: "Errors", key: "errors" }
+        ];
+        
+        const heading = [
+            "DegreeID",
+            "Name",
+            "SemesterID",
+            "Season",
+            "Year",
+            "Semester",
+            "CourseID",
+            "Course Listing",
+            "Course Title",
+            "PreReqs",
+            "CoReqs",
+            "Seasons Offered",
+            "Credits",
+            "Errors"
+        ].join(",");*/
+        const semesterString = degree.semesters
+            .map((semester: Semester) =>
+                [
+                    [
+                        semester.semesterID.toString(),
+                        semester.season,
+                        semester.year.toString(),
+                        semester.errors.join("\r\n")
+                    ].join(","),
+                    semester.courses
+                        .map((course: Course) =>
+                            [
+                                semester.semesterID.toString(),
+                                course.courseID.toString(),
+                                course.listing,
+                                course.title,
+                                course.preReqs.join("-").toString(),
+                                course.coReqs.join("-").toString(),
+                                course.offered.join("-"),
+                                course.credits.toString()
+                            ].join(",")
+                        )
+                        .join("\r\n")
+                ].join("\r\n")
+            )
+            .join("\r\n");
+        const degreeString = [
+            [degree.degreeID.toString(), degree.name].join(","),
+            semesterString
+        ].join("\r\n");
+        return degreeString;
+    }
+
+    function downloadBlob() {
+        // Create a blob
+        const blob = new Blob([arrayToCSV()], {
+            type: "text/csv;charset=utf-8;"
+        });
+        const url = URL.createObjectURL(blob);
+
+        // Create a link to download it
+        const pom = document.createElement("a");
+        pom.href = url;
+        pom.setAttribute(
+            "download",
+            ["degree", degree.degreeID, ".csv"].join("")
+        );
+        pom.click();
+    }
+
     return (
         <div hidden={hidden}>
+            <br></br>
+            <Button onClick={downloadBlob}>Export Degree Plan</Button>
             <br></br>
             <Button
                 onClick={() => setEditMode(!editMode)}
