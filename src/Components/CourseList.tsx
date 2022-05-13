716c31c8fe81068b53bedd96f65a211391126b7a
Michael Savas <mjsavas9@gmail.com>
2022-05-13 11:39:25 -0400

    added course list filter and tested it

--- src/Components/CourseList.tsx
+++ src/Components/CourseList.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Course } from "../Interfaces/course";
 import { CourseListView } from "./CourseListView";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 export function CourseList({
     courses,
@@ -10,16 +10,41 @@ export function CourseList({
     courses: Course[];
     editCourses: (courseID: number, editedCourse: Course) => void;
 }): JSX.Element {
-    const length = courses.length;
+    const [search, setSearch] = useState<string>("");
 
+    function updateSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        const newSearch = event.target.value;
+        setSearch(newSearch);
+        searchCourses(newSearch);
+    }
+
+    function searchCourses(search: string): Course[] {
+        return courses.filter((course: Course): boolean =>
+            course.listing.includes(search.toUpperCase())
+        );
+    }
     return (
         <div>
+            <div>
+                <Form.Group
+                    controlId="Course_listing_filter"
+                    className="container"
+                >
+                    <Form.Label>Filter course listing:</Form.Label>
+                    <Form.Control
+                        data-testid={"course-filter"}
+                        placeholder="Enter filter for courses here"
+                        value={search}
+                        onChange={updateSearch}
+                    />
+                </Form.Group>
+            </div>
             <Row>
                 <Col>
-                    {courses
+                    {searchCourses(search)
                         .filter(
                             (course: Course, index: number) =>
-                                index < length / 2
+                                index < searchCourses(search).length / 2
                         )
                         .map((course: Course) => (
                             <div key={course.courseID}>
@@ -32,10 +57,10 @@ export function CourseList({
                         ))}
                 </Col>
                 <Col>
-                    {courses
+                    {searchCourses(search)
                         .filter(
                             (course: Course, index: number) =>
-                                index >= length / 2
+                                index >= searchCourses(search).length / 2
                         )
                         .map((course: Course) => (
                             <div key={course.courseID}>
